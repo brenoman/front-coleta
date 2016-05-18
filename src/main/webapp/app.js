33b4f22da2fb4d4234636efec52d8ccc88729041
Breno <brenoman@gmail.com>
2016-05-18 00:57:26 -0300

    Modificações relativas aos formulários

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,10 +1,57 @@
+
 (function() {
   var app = angular.module("teste", []);
-  app.controller("ControllerPessoas", ["$http", function($http) {
-    this.pessoa = {};
-	this.addPessoaRest = function() {
-      $http.post('http://localhost:8080/hotelspring/alunos/salvar', this.aluno);
+  
+  app.controller("ControllerUsuario", ["$http", function($http) {
+    this.usuario = {};
+	this.addUsuarioRest = function() {
+      $http.post('http://localhost:8080/coletaSeletiva/usuarios/salvar', this.usuario);
+      alert("Cadastro efetuado");
+      window.location.reload();
     };
-	
+    
   }]);
+  
+
+  
+
+  app.controller("ControllerCooperativa", ["$http", function($http) {
+	    this.cooperativa = {};
+		this.addCooperativaRest = function() {
+	      $http.post('http://localhost:8080/coletaSeletiva/cooperativas/salvar', this.cooperativa);
+	      alert("Cadastro efetuado");
+	      window.location.reload();
+	    };
+		
+	  }]);
+  
+  
+  app.controller("ControllerColeta", ["$http", function($http) {
+
+	    this.coleta = {};
+		this.addColetaRest = function() {
+	      $http.post('http://localhost:8080/coletaSeletiva/coletas/salvar', this.coleta);
+	      alert("Cadastro efetuado");
+	      window.location.reload();
+	    };
+	    this.dbUsuarios  = [];
+	    this.atualizarUsuarios = function() {
+	      var that = this;
+	    	$http.get('http://localhost:8080/coletaSeletiva/usuarios/').success(function(data){
+	    	   that.dbUsuarios = data;
+	      });
+	    };
+	    this.dbCooperativas  = [];
+	    this.atualizarCooperativas = function() {
+		      var that = this;
+		    	$http.get('http://localhost:8080/coletaSeletiva/cooperativas/').success(function(data){
+		    	   that.dbCooperativas = data;
+		      });
+	    };
+	    this.atualizarUsuarios();
+	    this.atualizarCooperativas();
+	  }]);
+  
+  
+  
 })();
